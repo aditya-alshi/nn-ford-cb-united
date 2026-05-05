b759ecbf1cbac541aa78a521837ea3fd515ead3e
Aditya Alshi <alshiaditya55@gmail.com>
2026-05-05 14:25:46 +0530

    Tried footer at the bottom

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,16 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Slot } from 'expo-router'
+import { Slot, Stack, Link } from 'expo-router'
 
 const RootLayout = () => {
   return (
     <View style={{flex: 1}}>
-        <Slot></Slot>
-      <Text>Footer</Text>
+        <Stack></Stack>
+        <View style={styles.footer}>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+        </View>
     </View>
   )
 }
 
 export default RootLayout
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  footer: {
+    padding: 25,
+    flexDirection: "row",
+    justifyContent: "space-around"
+  }
+})
